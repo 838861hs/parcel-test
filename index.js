10fa8645ca1b197db7d2845bcd99b363317507d5
838861hs <philoaki0@gmail.com>
2024-04-11 16:57:45 +0900

    feat:parcel/react/MUIを追加

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Button } from '@mui/material';
+
+const App = () => (
+    <div>
+        <h1>Hello, React with MUI!</h1>
+        <Button variant="contained">Hello World</Button>
+    </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
