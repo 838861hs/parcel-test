a2b5391755d119870a3380263b0b8a8a7019d440
838861hs <philoaki0@gmail.com>
2024-04-25 21:35:01 +0900

    feat:トグルスイッチを追加

--- src/index.js
+++ src/index.js
@@ -2,38 +2,45 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'; 
 import { createRoot } from 'react-dom/client';
 import { Button,TextField,Checkbox } from '@mui/material';
+import React, { useState } from 'react';
 import PasswordInput from './components/password';
-import "./scss/index.scss";
-
-const App = () => (
-    <React.Fragment>
-        <div className='container'>
-
-        <h1>Hello, React with MUI!</h1>
-        <div>
-          <TextField 
-            label='test_input'
-            variant='outlined'
-            className='inputField' 
-            fullWidth/>
-        </div>
-        <div>
-          <PasswordInput  className="test_class"/>
-        </div>
-        <div>
-        <Button variant="contained">Hello World</Button>
-        </div>
-        </div>
-        <Checkbox/>
-    </React.Fragment>
-);
+import "../src/scss/index.scss";
+import { Switch, styled } from '@mui/material';
 
 const AppEml = document.getElementById('app');
 const AppRoot = createRoot(AppEml);
-AppRoot.render(<App />);
-console.log('test')
+AppRoot.render(<ToggleSwitch />);
+
+import React, { useState } from 'react';
+import { Switch, styled } from '@mui/material';
 
 
-//パスワード
-const paswordElm = document.getElementById('password');
-const passwordRoot = ReactDOM.createRoot(paswordElm);
+
+function ToggleSwitch() {
+  const [isToggled, setIsToggled] = useState(false);
+
+  const handleToggle = () => {
+    setIsToggled(!isToggled);
+  };
+
+  return (
+    <div className="toggle-switch">
+      <input
+        type="checkbox"
+        id="switch"
+        className="toggle-switch-checkbox"
+        onChange={handleToggle}
+        checked={isToggled}
+      />
+      <label className="toggle-switch-label" htmlFor="switch">
+        <span className="toggle-switch-inner" />
+        <span className="toggle-switch-switch" />
+      </label>
+    </div>
+  );
+}
+
+
+
+
+export default ToggleSwitch;
